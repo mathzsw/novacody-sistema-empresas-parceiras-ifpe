d06db0388125813fb69a16b0d9389211def534cd
Matheus Augusto <mara@discente.ifpe.edu.br>
2026-05-27 23:37:36 -0300

    Atualizando backend

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const mysql = require('mysql2');
-const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
 const conexao = mysql.createConnection({
@@ -15,11 +13,13 @@ const conexao = mysql.createConnection({
 });
 
 conexao.connect((erro) => {
+
     if (erro) {
-        console.log('Erro ao conectar no banco');
+        console.log('Erro ao conectar');
     } else {
         console.log('Banco conectado');
     }
+
 });
 
 app.get('/empresas', (req, res) => {
@@ -82,7 +82,60 @@ app.post('/empresa', (req, res) => {
         if (erro) {
             res.send('Erro ao cadastrar empresa');
         } else {
-            res.send('Empresa cadastrada com sucesso');
+            res.send('Empresa cadastrada');
+        }
+
+    });
+
+});
+
+app.put('/empresa/:id', (req, res) => {
+
+    const id = req.params.id;
+    const dados = req.body;
+
+    const sql = `
+        UPDATE empresa
+        SET
+            nome = ?,
+            cnpj = ?,
+            email = ?,
+            telefone = ?,
+            contato = ?,
+            endereco = ?,
+            tipo_parceria = ?,
+            website = ?,
+            quantidade_vagas = ?,
+            descricao_vagas = ?,
+            aceita_estagiario = ?,
+            observacoes = ?,
+            status_empresa = ?
+        WHERE id_empresa = ?
+    `;
+
+    const valores = [
+        dados.nome,
+        dados.cnpj,
+        dados.email,
+        dados.telefone,
+        dados.contato,
+        dados.endereco,
+        dados.tipo_parceria,
+        dados.website,
+        dados.quantidade_vagas,
+        dados.descricao_vagas,
+        dados.aceita_estagiario,
+        dados.observacoes,
+        dados.status_empresa,
+        id
+    ];
+
+    conexao.query(sql, valores, (erro) => {
+
+        if (erro) {
+            res.send('Erro ao editar');
+        } else {
+            res.send('Empresa editada');
         }
 
     });
@@ -102,36 +155,17 @@ app.put('/empresa/aprovar/:id', (req, res) => {
     conexao.query(sql, [id], (erro) => {
 
         if (erro) {
-            res.send('Erro ao aprovar empresa');
+            res.send('Erro ao aprovar');
         } else {
-            res.send('Empresa aprovada com sucesso');
+            res.send('Empresa aprovada');
         }
 
     });
 
 });
 
-app.delete('/empresa/:id', (req, res) => {
 
-    const id = req.params.id;
-
-    const sql = `
-        DELETE FROM empresa
-        WHERE id_empresa = ?
-    `;
-
-    conexao.query(sql, [id], (erro) => {
-
-        if (erro) {
-            res.send('Erro ao deletar empresa');
-        } else {
-            res.send('Empresa deletada com sucesso');
-        }
-
-    });
-
-});
 
 app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
+    console.log('Servidor rodando');
 });
