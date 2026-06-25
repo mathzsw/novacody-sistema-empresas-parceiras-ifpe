a85ec642b6157802e5f66c9cc68223ceae4c28b8
mathzsw <mara@discente.ifpe.edu.br>
2026-06-25 06:41:02 -0300

    Refactor to use Sequelize for database operations

--- servidor.js
+++ servidor.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const mysql = require('mysql2');
+const sequelize = require('./config/bd');
+const Empresa = require('./models/Empresa');
 
 const app = express();
 
@@ -10,179 +11,95 @@ app.use(express.urlencoded({ extended: true }));
 app.engine('handlebars', exphbs.engine({ defaultLayout: false }));
 app.set('view engine', 'handlebars');
 
-const conexao = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'ifpecjbg',
-    database: 'novacody'
-});
+sequelize.sync()
+    .then(() => {
+        console.log('Banco conectado!');
+    })
+    .catch((erro) => {
+        console.log('Erro ao conectar:', erro);
+    });
 
-conexao.connect((erro) => {
-    if (erro) {
-        console.log("ERRO COMPLETO:", erro);
-    } else {
-        console.log("Conectado com sucesso!");
+app.get('/empresas', async (req, res) => {
+    try {
+        const empresas = await Empresa.findAll();
+        res.json(empresas);
+    } catch (erro) {
+        res.send('Erro ao buscar empresas');
     }
 });
 
-app.get('/empresas', (req, res) => {
+app.post('/empresa', async (req, res) => {
+    try {
+        await Empresa.create({
+            nome: req.body.nome,
+            cnpj: req.body.cnpj,
+            email: req.body.email,
+            telefone: req.body.telefone,
+            contato: req.body.contato,
+            endereco: req.body.endereco,
+            tipo_parceria: req.body.tipo_parceria,
+            website: req.body.website,
+            quantidade_vagas: req.body.quantidade_vagas,
+            descricao_vagas: req.body.descricao_vagas,
+            aceita_estagiario: req.body.aceita_estagiario,
+            observacoes: req.body.observacoes,
+            status_empresa: 'pendente'
+        });
 
-    const sql = 'SELECT * FROM empresa';
-
-    conexao.query(sql, (erro, resultado) => {
-
-        if (erro) {
-            res.send('Erro ao buscar empresas');
-        } else {
-            res.json(resultado); // CORRIGIDO
-        }
-
-    });
-
+        res.send('Empresa cadastrada');
+    } catch (erro) {
+        res.send('Erro ao cadastrar empresa');
+    }
 });
 
-app.post('/empresa', (req, res) => {
+app.put('/empresa/:id', async (req, res) => {
+    try {
+        const empresa = await Empresa.findByPk(req.params.id);
 
-    const dados = req.body;
-
-    const sql = `
-        INSERT INTO empresa (
-            nome,
-            cnpj,
-            email,
-            telefone,
-            contato,
-            endereco,
-            tipo_parceria,
-            website,
-            quantidade_vagas,
-            descricao_vagas,
-            aceita_estagiario,
-            observacoes,
-            status_empresa
-        )
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    `;
-
-    const valores = [
-        dados.nome,
-        dados.cnpj,
-        dados.email,
-        dados.telefone,
-        dados.contato,
-        dados.endereco,
-        dados.tipo_parceria,
-        dados.website,
-        dados.quantidade_vagas,
-        dados.descricao_vagas,
-        dados.aceita_estagiario,
-        dados.observacoes,
-        'pendente'
-    ];
-
-    conexao.query(sql, valores, (erro) => {
-
-        if (erro) {
-            res.send('Erro ao cadastrar empresa');
-        } else {
-            res.send('Empresa cadastrada');
+        if (!empresa) {
+            return res.send('Empresa não encontrada');
         }
 
-    });
+        await empresa.update(req.body);
 
+        res.send('Empresa editada');
+    } catch (erro) {
+        res.send('Erro ao editar empresa');
+    }
 });
 
-app.put('/empresa/:id', (req, res) => {
+app.put('/empresa/aprovar/:id', async (req, res) => {
+    try {
+        const empresa = await Empresa.findByPk(req.params.id);
 
-    const id = req.params.id;
-    const dados = req.body;
-
-    const sql = `
-        UPDATE empresa
-        SET
-            nome = ?,
-            cnpj = ?,
-            email = ?,
-            telefone = ?,
-            contato = ?,
-            endereco = ?,
-            tipo_parceria = ?,
-            website = ?,
-            quantidade_vagas = ?,
-            descricao_vagas = ?,
-            aceita_estagiario = ?,
-            observacoes = ?,
-            status_empresa = ?
-        WHERE id_empresa = ?
-    `;
-
-    const valores = [
-        dados.nome,
-        dados.cnpj,
-        dados.email,
-        dados.telefone,
-        dados.contato,
-        dados.endereco,
-        dados.tipo_parceria,
-        dados.website,
-        dados.quantidade_vagas,
-        dados.descricao_vagas,
-        dados.aceita_estagiario,
-        dados.observacoes,
-        dados.status_empresa,
-        id
-    ];
-
-    conexao.query(sql, valores, (erro) => {
-
-        if (erro) {
-            res.send('Erro ao editar empresa');
-        } else {
-            res.send('Empresa editada');
+        if (!empresa) {
+            return res.send('Empresa não encontrada');
         }
 
-    });
+        empresa.status_empresa = 'aprovada';
 
+        await empresa.save();
+
+        res.send('Empresa aprovada');
+    } catch (erro) {
+        res.send('Erro ao aprovar empresa');
+    }
 });
 
-app.put('/empresa/aprovar/:id', (req, res) => {
+app.delete('/empresa/:id', async (req, res) => {
+    try {
+        const empresa = await Empresa.findByPk(req.params.id);
 
-    const id = req.params.id;
-
-    const sql = `
-        UPDATE empresa
-        SET status_empresa = 'aprovada'
-        WHERE id_empresa = ?
-    `;
-
-    conexao.query(sql, [id], (erro) => {
-
-        if (erro) {
-            res.send('Erro ao aprovar empresa');
-        } else {
-            res.send('Empresa aprovada');
+        if (!empresa) {
+            return res.send('Empresa não encontrada');
         }
 
-    });
+        await empresa.destroy();
 
-});
-
-app.delete('/empresa/:id', (req, res) => {
-
-    const id = req.params.id;
-
-    const sql = 'DELETE FROM empresa WHERE id_empresa = ?';
-
-    conexao.query(sql, [id], (erro) => {
-
-        if (erro) {
-            res.send('Erro ao deletar empresa');
-        } else {
-            res.send('Empresa removida');
-        }
-
-    });
-
+        res.send('Empresa removida');
+    } catch (erro) {
+        res.send('Erro ao deletar empresa');
+    }
 });
 
 app.listen(3000, () => {
